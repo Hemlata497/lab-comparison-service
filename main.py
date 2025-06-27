9cecac6f505390bdfe7d810df9f1e8802ec61df4
Hemlata <ht9647215@gmail.com>
2025-06-27 13:18:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,5 +17,8 @@ app.add_middleware(
 # Include the comparison routes
 app.include_router(compare_router, tags=["compare"])
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
